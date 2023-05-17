7c4e07b363e72bae659aab02add11b643188e78e
Gudeta Buli <gbteshite6195@gmail.com>
2023-05-17 13:05:37 +0300

    made some changes

--- src/componets/Product.jsx
+++ src/componets/Product.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 
-import fincare from './images/miko.jpg'
-import checkl from './images/checkL.png'
+import fincare from "./images/miko.jpg";
+import checkl from "./images/checkL.png";
 
 function Product() {
+  const about = [
+    {
+      id: 1,
+      p1: "Multi Channel",
+      desc: "20 channels, detectmore items one time",
+    },
+    {
+      id: 2,
+      p1: "Reliable Accuracy",
+      desc: "Good correlation with CLIA method",
+    },
+    {
+      id: 3,
+      p1: "Temprature control",
+      desc: "Provide better reaction conditione",
+    },
+    {
+      id: 4,
+      p1: "Rapid Test",
+      desc: "High speed of detection,enable 200 tests per hour",
+    },
+  ];
+
   return (
     <div className="m-3 h-auto sm:m- md:m-40 md:my-1 md:mt-10 rounded-lg ">
       <div className="">
         <img
-          className="h-64 sm:h-96   w-[100%] rounded lg:h-fit border"
+          className="h-64 sm:h-96 w-[100%] rounded lg:h-fit border"
           src={fincare}
           alt="lean-img"
         />
@@ -17,38 +40,20 @@ function Product() {
             FIA Meter III Plus (FS-205)
           </h2>
           <p className="font-light mx-6 ">
-           The FIA Meter III Plus (FS-205) is a specific model of a fluorescence immunoassay (FIA) meter.  </p> 
-            <div className="p-0 lg:p-2 my-1">
-              <div className="flex p-2">
-                <img src={checkl} className="w-6 h-6  " alt="multi" />
-                <p className="ml-10  flex-col absolute font-light ">Multi-Channel</p>
-                <p className="mt-7 ml-4 font-thin ">20 channels, detect
-                   more items one time</p>
-                
-              </div>
-              <div className="flex p-2">
-                <img src={checkl} className="w-6 h-6 " alt="multi" />
-                <p className="ml-10  flex-col absolute font-light ">Reliable Accuracy</p>
-                <p className="mt-7 ml-4 font-thin ">Good correlation
-                                                    with CLIA method</p>
-              </div>
-              <div className="flex p-2">
-                <img src={checkl} className="w-6 h-6 " alt="multi" />
-                <p className="ml-10  flex-col absolute font-light ">Temperature control</p>
-                <p className="mt-7 ml-4 font-thin ">Provide better
-                                                    reaction condition</p>
-              </div>
-               <div className="flex p-2">
-            <img src={checkl} className="w-6 h-6 " alt="multi" />
-                <p className="ml-10  flex-col absolute font-light ">Rapid Test</p>
-                <p className="mt-7 ml-4 font-thin ">High speed of detection,
-                                                     enable 200 tests per hour</p>
-              </div>
-            </div>
-        
-          {/* <button className="mt-8 lg:mt-0 bg-[#1782AA] p-2 rounded hover:bg-cyan-700 text-white">
-            LEARN MORE
-          </button> */}
+            The FIA Meter III Plus (FS-205) is a specific model of a
+            fluorescence immunoassay (FIA) meter.{" "}
+          </p>
+          <div className="p-0 lg:p-2 my-1">
+            {about.map(({ id, p1, desc }) => {
+              return (
+                <div className="flex p-2" key={id}>
+                  <img src={checkl} className="w-6 h-6" alt="multi" />
+                  <p className="ml-10  flex-col absolute font-light ">{p1}</p>
+                  <p className="mt-7 ml-4 font-thin ">{desc}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
